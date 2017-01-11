f0986487e951088093e449c14bbdd2edd3813300
songwei <songwei@xiaohongchun.com>
2017-01-11 13:44:29 +0800

    update/add cache for HandlerChain

--- src/main/java/org/hddframework/mybatis/locker/handler/HandlerFactory.java
+++ src/main/java/org/hddframework/mybatis/locker/handler/HandlerFactory.java
@@ -1,42 +1,51 @@
 package org.hddframework.mybatis.locker.handler;
 
-import java.util.Objects;
-
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.hddframework.mybatis.locker.refactor.SQLRefactor;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by SongWei on 01/12/2016.
  */
 public class HandlerFactory {
+    private static final Map<Class, HandlerChain> handlersCache = new ConcurrentHashMap<>(16);
 
-	public static SQLRefactor build(String column, String property) {
-		DefaultHandler defaultHandler = new DefaultHandler();
-		SQLRefactorHandler deleteHandler = new DeleteHandler(column, property, defaultHandler);
-		SQLRefactorHandler updateHandler = new UpdateHandler(column, property, deleteHandler);
-		SQLRefactorHandler insertHandler = new InsertHandler(column, property, updateHandler);
-		SQLRefactorHandler selectHandler = new SelectHandler(column, property, insertHandler);
-		SQLRefactorHandler complexHandler = new ComplexHandler(column, property, selectHandler);
-		HandlerChain chain = new HandlerChain(complexHandler);
-		return chain;
-	}
+    public static SQLRefactor build(String column, String property) {
+        HandlerChain sqlRefactorHandler = handlersCache.get(HandlerChain.class);
+        if (Objects.isNull(sqlRefactorHandler)) {
+            DefaultHandler defaultHandler = new DefaultHandler();
+            SQLRefactorHandler deleteHandler = new DeleteHandler(column, property, defaultHandler);
+            SQLRefactorHandler updateHandler = new UpdateHandler(column, property, deleteHandler);
+            SQLRefactorHandler insertHandler = new InsertHandler(column, property, updateHandler);
+            SQLRefactorHandler selectHandler = new SelectHandler(column, property, insertHandler);
+            SQLRefactorHandler complexHandler = new ComplexHandler(column, property, selectHandler);
+            HandlerChain chain = new HandlerChain(complexHandler);
 
-	private static class HandlerChain implements SQLRefactorHandler {
+            handlersCache.put(HandlerChain.class, chain);
+            sqlRefactorHandler = chain;
+        }
+        return sqlRefactorHandler;
+    }
 
-		private final SQLRefactorHandler next;
+    private static class HandlerChain implements SQLRefactorHandler {
 
-		public HandlerChain(SQLRefactorHandler next) {
-			this.next = next;
-		}
+        private final SQLRefactorHandler next;
 
-		@Override
-		public String refactorSQL(String originalSQL, StatementHandler statementHandler) {
-			if (!Objects.isNull(next)) {
-				return next.refactorSQL(originalSQL, statementHandler);
-			}
-			return null;
-		}
+        public HandlerChain(SQLRefactorHandler next) {
+            this.next = next;
+        }
 
-	}
+        @Override
+        public String refactorSQL(String originalSQL, StatementHandler statementHandler) {
+            if (!Objects.isNull(next)) {
+                return next.refactorSQL(originalSQL, statementHandler);
+            }
+            return null;
+        }
+
+    }
 
 }
